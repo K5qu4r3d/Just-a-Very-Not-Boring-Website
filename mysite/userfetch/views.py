f5aa18c764d1bc42357fe1eaeeceac15171cb251
Kevin Keomalaythong <kkeomalaythong117@gmail.com>
2019-06-08 17:11:40 -0400

    Added User object create-and-save function

--- mysite/userfetch/views.py
+++ mysite/userfetch/views.py
@@ -6,3 +6,12 @@ def register(request):
     
 def index(request):
     return HttpResponse("Hello, world. You're at the login page.")
+
+def fetch(request):
+	User.objects.create(first_name=request.POST['first_name'], 
+						last_name=request.POST['last_name'], 
+						email=request.POST['email'], 
+						CCN=request.POST['CCN'], 
+						phone=request.POST['phone'], 
+						address=request.POST['address'], 
+						password=request.POST['password'])
