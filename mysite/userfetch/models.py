2b5f697fc3f446c4fe549b3df9f6c72fb36328b4
TJ Fallon <tjfallon@ncsu.edu>
2019-06-08 15:01:58 -0400

    Created basic database table

--- mysite/userfetch/models.py
+++ mysite/userfetch/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=200)
+    CCN = models.CharField(max_length=16)
+    phone = models.CharField(max_length=30)
+    address = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
